038d40af5bf19a5331899b2775dd0f539ba5422e
Aravind <Aravindbalan222@gmail.com>
2018-03-31 15:07:10 +0530

    second commit

--- app/src/main/java/com/code_blooded/mrithyu_care/afterdeathcare/Main2Activity.java
+++ app/src/main/java/com/code_blooded/mrithyu_care/afterdeathcare/Main2Activity.java
@@ -1,26 +1,31 @@
 package com.code_blooded.mrithyu_care.afterdeathcare;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
+/**
+ * Created by ssaurel on 02/12/2016.
+ */
 public class Main2Activity extends AppCompatActivity {
-
+    private static int SPLASH_TIME_OUT = 2000;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        ImageView mImageView = (ImageView)findViewById(R.id.arrow_left);
-        mImageView.setOnClickListener(new View.OnClickListener() {
+        // Showing splash screen with a timer.
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),MainActivity.class);
+            public void run() {
+                // Start your application main_activity
+                Intent i = new Intent(Main2Activity.this, MainActivity.class);
                 startActivity(i);
 
+                // Close this activity
+                finish();
             }
-        });
+        }, SPLASH_TIME_OUT); // Timer
     }
-}
+    }
